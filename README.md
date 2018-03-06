be0122a0f15c45d7a9dc60cb84035e098a88d9aa
David Lac <kiddmit3@gmail.com>
2018-03-06 14:22:54 -0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PremierAcademyArcadia
-Static business page for Premier Academy Arcadia. Using Materialize for frontend and Handlebars.js for templating.
+# Premier Academy Arcadia
+Static business landing page for Premier Academy Arcadia. Using Materialize for frontend and Handlebars.js for templating.
---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+ var teachers = [
+ 	{
+ 		name: "David Lac",
+ 		university: "UC Davis",
+ 		degree: "Bachelor's of Science",
+ 		subjects: "Math, Biology, Chemistry",
+ 		profile: "./assets/img/profiles/david-lac.jpg",
+ 		bio: ""
+ 	}
+ ];
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+ $(".button-collapse").sideNav();
