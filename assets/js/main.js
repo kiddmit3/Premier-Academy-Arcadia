e938f27c3fcc5d8c49fc6047771e2491c2f0056e
David Lac <kiddmit3@gmail.com>
2018-03-07 13:22:33 -0800

    form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
- var teachers = [
- 	{
- 		name: "David Lac",
- 		university: "UC Davis",
- 		degree: "Bachelor's of Science",
- 		subjects: "Math, Biology, Chemistry",
- 		profile: "./assets/img/profiles/david-lac.jpg",
- 		bio: ""
- 	}
- ];
+ var teachers = [{
+     name: "David Lac",
+     university: "UC Davis",
+     degree: "Bachelor's of Science",
+     subjects: "Math, Biology, Chemistry",
+     profile: "./assets/img/profiles/david-lac.jpg",
+     bio: ""
+ }];
 
-    $(document).ready(function(){
-      $('.parallax').parallax();
-    });
 
  $(".button-collapse").sideNav();
+
+
+ $(document).ready(function() {
+     $('.parallax').parallax();
+     $('.modal').modal();
+     $('select').material_select();
+     $('.scrollspy').scrollSpy();
+ });
