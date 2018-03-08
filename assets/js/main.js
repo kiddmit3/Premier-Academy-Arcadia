1aeb821dc9ae6a48d85b70adc0042aaf5650a264
David Lac <kiddmit3@gmail.com>
2018-03-07 23:21:48 -0800

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,56 @@
      $('select').material_select();
      $('.scrollspy').scrollSpy({ scrollOffset: 55 });
  });
+
+ var animateHTML1 = function () {
+  var elems,
+    windowHeight
+  var init = function () {
+    elems = document.getElementsByClassName('hiddenRight')
+    windowHeight = window.innerHeight
+    _addEventHandlers()
+  }
+  var _addEventHandlers = function () {
+    window.addEventListener('scroll', _checkPosition)
+    window.addEventListener('resize', init)
+  }
+  var _checkPosition = function () {
+    for (var i = 0; i < elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().top
+      if (posFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace('hiddenRight', 'fadeRight')
+      }
+    }
+  }
+  return {
+    init: init
+  }
+}
+
+ var animateHTML2 = function () {
+  var elems,
+    windowHeight
+  var init = function () {
+    elems = document.getElementsByClassName('hiddenLeft')
+    windowHeight = window.innerHeight
+    _addEventHandlers()
+  }
+  var _addEventHandlers = function () {
+    window.addEventListener('scroll', _checkPosition)
+    window.addEventListener('resize', init)
+  }
+  var _checkPosition = function () {
+    for (var i = 0; i < elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().top
+      if (posFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace('hiddenLeft', 'fadeLeft')
+      }
+    }
+  }
+  return {
+    init: init
+  }
+}
+
+animateHTML1().init();
+animateHTML2().init()
